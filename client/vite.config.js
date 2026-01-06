e93856146eb4037a958aacf8fc890d7d2825fa00
Dhirendra vishwakarma <dhiru7521071887@gmail.com>
2026-01-06 19:14:34 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://lms-ba.vercel.app/',
         changeOrigin: true,
         secure: false,
       },
